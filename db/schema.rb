4da3c4b0fd027bfc8020803072194c5e5e0a13ae
Michael <Mikeks81@gmail.com>
2016-02-05 17:28:30 -0500

    Adding comments, users, admin toolbar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204153957) do
+ActiveRecord::Schema.define(version: 20160205221352) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "body"
+    t.string   "name"
+    t.boolean  "flag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "report_id"
+  end
+
+  add_index "comments", ["report_id"], name: "index_comments_on_report_id"
 
   create_table "reports", force: :cascade do |t|
     t.string   "title"
@@ -22,9 +33,9 @@ ActiveRecord::Schema.define(version: 20160204153957) do
     t.boolean  "stalking"
     t.boolean  "threat"
     t.boolean  "touching"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.boolean  "comments"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.boolean  "mean_comments"
     t.boolean  "exposure"
     t.boolean  "assault"
     t.boolean  "rape"
@@ -32,6 +43,14 @@ ActiveRecord::Schema.define(version: 20160204153957) do
     t.boolean  "gestures"
     t.boolean  "takingphotos"
     t.string   "address"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.boolean  "admin"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end

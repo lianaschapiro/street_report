e61afb57b980dfa4f1075b377c82fd723a5962f4
lianaschapiro <liana.schapiro@gmail.com>
2016-02-09 15:13:38 -0500

    Insignificant

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email: "liana.schapiro@gmail.com", password: "1234")
+

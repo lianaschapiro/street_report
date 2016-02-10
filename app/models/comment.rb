96471ff4b1204130e61dd653c3dc33c0e07362aa
lianaschapiro <liana.schapiro@gmail.com>
2016-02-10 11:30:16 -0500

    Added model validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ActiveRecord::Base
 	belongs_to :report
 
+	validates_presence_of :name, :body
+
 	include Humanizer
   	require_human_on :create
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
 	has_secure_password
+	
+	validates :email, presence: true
+	validates :password, presence: true, length: {in: 4..20}, on: :create
 end
